4f6d83bf2e66facf1e31a6b5ae6797e55c19fa61
Steve Gelbard <gelbardsteve@gmail.com>
2019-06-07 23:00:14 +0300

    message

---
+++ userObject.js
@@ -0,0 +1,82 @@
+var object = [
+  {
+    img: "imgs/john-smith.jpg",
+    position: "Graphics designer",
+    name: "John Smith",
+    place: "Riviera State",
+    twitter: "Twitter, Inc",
+    description: "795 Folsom Ave, Suite 600 San Francisco, CA 94107",
+    Telephon: "(123) 456-7890",
+    delete: "Delet Button"
+  },
+  {
+    img: "imgs/alex-jonathan.jpg",
+    position: "CEO",
+    name: "Alex Johnatan",
+    place: "Riviera State",
+    twitter: "Twitter, Inc",
+    description: "795 Folsom Ave, Suite 600 San Francisco, CA 94107",
+    Telephon: "(123) 456-7890",
+    delete: "Delet Button"
+  },
+  {
+    img: "imgs/monica-smith.jpg",
+    position: "Marketing manager",
+    name: "Monica Smith",
+    place: "Riviera State",
+    twitter: "Twitter, Inc",
+    description: "795 Folsom Ave, Suite 600 San Francisco, CA 94107",
+    Telephon: "(123) 456-7890",
+    delete: "Delet Button"
+  },
+  {
+    img: "imgs/michael-zimber.jpg",
+    position: "Sales manager",
+    name: "Michael Zimber",
+    place: "Riviera State",
+    twitter: "Twitter, Inc",
+    description: "795 Folsom Ave, Suite 600 San Francisco, CA 94107",
+    Telephon: "(123) 456-7890",
+    delete: "Delet Button"
+  },
+  {
+    img: "imgs/sandra-smith.jpg",
+    position: "Graphics designer",
+    name: "Sandra Smith",
+    place: "Riviera State",
+    twitter: "Twitter, Inc",
+    description: "795 Folsom Ave, Suite 600 San Francisco, CA 94107",
+    Telephon: "(123) 456-7890",
+    delete: "Delet Button"
+  },
+  {
+    img: "imgs/janeth-carton.jpg",
+    position: "Graphics designer",
+    name: "Janet Cartom",
+    place: "Riviera State",
+    twitter: "Twitter, Inc",
+    description: "795 Folsom Ave, Suite 600 San Francisco, CA 94107",
+    Telephon: "(123) 456-7890",
+    delete: "Delet Button"
+  },
+  {
+    img: "imgs/alex-jonathan.jpg",
+    position: "CEO",
+    name: "Alex Johnatan",
+    place: "Riviera State",
+    twitter: "Twitter, Inc",
+    description: "795 Folsom Ave, Suite 600 San Francisco, CA 94107",
+    Telephon: "(123) 456-7890",
+    delete: "Delet Button"
+  },
+  {
+    img: "imgs/john-smith.jpg",
+    position: "Graphics designer",
+    name: "John Smith",
+    place: "Riviera State",
+    twitter: "Twitter, Inc",
+    description: "795 Folsom Ave, Suite 600 San Francisco, CA 94107",
+    Telephon: "(123) 456-7890",
+    delete: "Delet Button"
+  }
+];
